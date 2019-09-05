01130e25ba940be71bc4348a1a0a409f4c21fa46
Steven <parrsm18@wfu.edu>
2019-09-05 15:29:40 -0400

    spaces

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "    Hello, World!" << std::endl;
     return 0;
 }
